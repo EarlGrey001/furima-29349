0bae30cbbfd5a37ae81c9650d034e27bb34b2055
earlgrey01 <sakayori0817@yahoo.co.jp>
2020-09-10 14:47:41 +0900

    バリデーションとテストコード1

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname, presence: true, length: { maximum: 6 }
+  validates :email, presence: true, uniqueness: true, inclusion: { in: "@" }
+  validates :password, presence: true, length: { maximum: 6 }
+  validates :password_confirmation, presence: true, length: { maximum: 6 }
+  validates :family_name, presence: true, length: { maximum: 6 }
+  validates :first_name, presence: true, length: { maximum: 6 }
+  validates :family_name_kana, presence: true, length: { maximum: 6 }
+  validates :first_name_kana, presence: true, length: { maximum: 6 }
+  validates :birthday, presence: true
 end
