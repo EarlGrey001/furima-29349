e6a5279315b59c45508c20a56c499ae12a2feac7
earlgrey01 <sakayori0817@yahoo.co.jp>
2020-09-11 17:15:42 +0900

    Rubocop実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   validates :nickname, presence: true
   validates :password, presence: true, length: { minimum: 6 }
   validates_format_of :password, with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
