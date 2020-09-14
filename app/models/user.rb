5c1ab1e8e76f2ca9fa1d95843f4b31fe12a7a2da
earlgrey01 <sakayori0817@yahoo.co.jp>
2020-09-14 12:20:58 +0900

    ログイン者のみ出品可

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :items
+
 
   with_options presence: true do
     validates :nickname
@@ -19,6 +21,6 @@ class User < ApplicationRecord
     validates_format_of :first_name_kana, with: /\A[ァ-ン]+\z/
     validates :birthday
 
-    #has_many :items
+    
   end
 end
