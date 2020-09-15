b39c5563bed1ebcaaf8a0430b9f66025a6d85910
earlgrey01 <sakayori0817@yahoo.co.jp>
2020-09-15 15:01:05 +0900

    各項目の設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
   def new
   end
+
+  def create
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,38 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  has_one_attached :image
+
+  validates :price, presence: true
+  validates_format_of :price, with: /\A\d+\z/
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+
+
+  private
+
+  def item_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  validates :title, :text, :genre, presence: true
+  validates :genre_id, numericality: { other_than: 1 } 
+
+  belongs_to_active_hash :state
+  validates :title, :text, :genre, presence: true
+  validates :genre_id, numericality: { other_than: 1 } 
+
+  belongs_to_active_hash :cost
+  validates :title, :text, :genre, presence: true
+  validates :genre_id, numericality: { other_than: 1 } 
+
+  belongs_to_active_hash :area
+  validates :title, :text, :genre, presence: true
+  validates :genre_id, numericality: { other_than: 1 } 
+
+  belongs_to_active_hash :day
+  validates :title, :text, :genre, presence: true
+  validates :genre_id, numericality: { other_than: 1 } 
+ 
 end
