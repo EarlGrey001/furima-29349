c29990ed44e40b1996f62c32df02c6ab688f6659
earlgrey01 <sakayori0817@yahoo.co.jp>
2020-09-17 11:05:07 +0900

    修正2+

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,13 +3,24 @@ class Item < ApplicationRecord
 
   has_one_attached :image
 
-  validates :name, :price, :comment, :image, presence: true
+
+  with_options presence: true do
+    validates :name
+    validates :price
+    validates :comment
+    validates :image
+    validates :shipping_state_id
+    validates :category_id
+    validates :shipping_cost_id
+    validates :shipping_origin_id
+    validates :shipping_days_id
+  end
+
   validates_format_of :price, with: /\A\d+\z/
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
   extend ActiveHash::Associations::ActiveRecordExtensions
 
-  validates :shipping_cost_id, :shipping_days_id, :shipping_state_id, :shipping_origin_id, :category_id, presence: true
 
   with_options numericality: { other_than: 1 } do
     validates :shipping_state_id
