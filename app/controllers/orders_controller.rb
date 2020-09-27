b512f41c9eaee24f3e5bb1d4ad8473b549d986c4
earlgrey01 <sakayori0817@yahoo.co.jp>
2020-09-27 09:22:25 +0900

    修正3

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -28,15 +28,12 @@ class OrdersController < ApplicationController
 
   def order_params
     params.permit(:item_id, :postal_code, :area_id, :city, :building, :address, :phone_number, :token).merge(user_id: current_user.id)
-    # ストロングパラメーターにitem_idのキーを設定しましょう
   end
 
   def pay_item
     Payjp.api_key = ENV['PAYJP_SECRET_KEY']
-    Payjp.api_key = 'sk_test_78f584725630c8fd97049abf' # PAY.JPテスト秘密鍵
     Payjp::Charge.create(
       amount: @item.price, # 商品の値段
-      # @itemの中にpriceの情報が入っているので＠itemからpriceを抜き出しましょう
       card: order_params[:token], # カードトークン
       currency: 'jpy'                 # 通貨の種類(日本円)
     )
